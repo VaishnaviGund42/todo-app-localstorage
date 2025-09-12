3d7f4cec5f9960cc82f4e8c03ce216aa08f63354
VaishnaviGund42 <vaishnavigund42@gmail.com>
2025-09-12 18:41:01 +0530

    local storage function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
  const todoForm = document.getElementById('todo-form');
         const todoInput = document.getElementById('todo-input');
         const todoList = document.getElementById('todo-list');
+         let todos = JSON.parse(localStorage.getItem('todos')) || [];
+
